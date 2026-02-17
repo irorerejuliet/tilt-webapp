5e2ba4d61fc5d2177edaa79dd60fb3579c588986
irorere juliet <irorerejuliet@gmail.com>
2026-02-17 23:28:37 +0100

    about

--- src/app/company/about/page.tsx
+++ src/app/company/about/page.tsx
@@ -1,9 +1,17 @@
 import Explaination from "@/components/Explaination";
+import Funding from "@/components/Funding";
 
 const page = () => {
   return (
     <div>
       <Explaination />
+      <Funding
+        title="People"
+        subTitle="are"
+        description="incredible"
+        button="Get started"
+        image="/images/people-are-incrediblw.webp"
+      />
     </div>
   );
 };
